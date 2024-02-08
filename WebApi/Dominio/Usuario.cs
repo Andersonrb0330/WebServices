6b0db8d41ce09aaec204ce009a4b00d34aee7a84
Andersonrb0330 <andersonrb0330@gmail.com>
2024-02-08 11:01:33 -0500

    Agregue Clase Usuario

---
+++ WebApi/Dominio/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebApi.Dominio
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public int Telefono { get; set; }
+
+
+        public Usuario()
+        {
+        }
+
+    }
+}
+
