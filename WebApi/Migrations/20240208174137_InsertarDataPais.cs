d1017056a9ebb73eb4ff329cbec6a69f95d71d1c
Andersonrb0330 <andersonrb0330@gmail.com>
2024-02-08 12:52:07 -0500

    Se inserto datos a la tabla Pais

---
+++ WebApi/Migrations/20240208174137_InsertarDataPais.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertarDataPais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert Into Paises Values ('Perù'), ('Paraguay'), ('Brazil' )");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Paises");
+        }
+    }
+}
